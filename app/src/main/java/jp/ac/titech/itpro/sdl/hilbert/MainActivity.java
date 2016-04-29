4e47a03ac48dc430fd5a11571171fdb82403c4c2
patriotxx <yamagata@taigas-MacBook-Pro.local>
2016-04-29 16:22:47 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -0,0 +1,89 @@
+package jp.ac.titech.itpro.sdl.hilbert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.util.*;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView levelView;
+    private HilbertView hilbertView;
+    private final static int MAX_N = 9;
+    private int n = 1;
+    private static final String TAG = "MainActivity";
+    private final static String KEY_N = "MainActivity.n";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+        levelView = (TextView)findViewById(R.id.level_view);
+        hilbertView = (HilbertView)findViewById(R.id.hilbert_view);
+
+        if(savedInstanceState != null){
+            n = savedInstanceState.getInt(KEY_N);
+
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_N, n);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        display();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    public void onClickDec(View v) {
+        if (n > 1) n--;
+        display();
+    }
+
+    public void onClickInc(View v) {
+        if (n < MAX_N) n++;
+        display();
+
+    }
+
+    private void display() {
+        levelView.setText(getString(R.string.level_format, n));
+        hilbertView.setLevel(n);
+    }
+}
